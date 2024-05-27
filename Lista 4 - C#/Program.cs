643652f344996e6729dcf893d5b7f5cb99b802f1
SkSamuel157 <samuelscs12senai@gmail.com>
2024-05-27 09:25:13 -0300

    feito

---
+++ Lista 4 - C#/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace lacoDeRepeticao
+{
+    class projeto
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                /*{
+                                Console.WriteLine("Informe um número para ver sua tabuada:");
+                                int numero = int.Parse(Console.ReadLine());
+
+                                for (int i = 1; i <= 10; i++)
+                                {
+                                    Console.WriteLine(numero * i);
+                                }
+                            }*/
+
+                /*{
+                    Console.WriteLine("Digite um número:");
+                    int numero = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i <= numero; i += 3)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }*/
+
+                /*{
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("Digite um número:");
+                        int numero = int.Parse(Console.ReadLine());
+
+                        if (numero > 100)
+                        {
+                            Console.WriteLine("NÚMERO ESPECIAL");
+                        }
+                        else
+                        {
+                            Console.WriteLine("NÚMERO COMUM");
+                        }
+                    }
+                }*/
+
+                /*{
+                    string resposta;
+                    do
+                    {
+                        Console.WriteLine("Deseja continuar? (s/n)");
+                        resposta = Console.ReadLine().ToLower();
+                    } while (resposta == "s");
+                }*/
+            }
+        }
+    }
